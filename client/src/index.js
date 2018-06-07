2617853ae016433a2b5f885f693bba5ecc8e8864
greg.mcgough <greg.mcgough@servicenow.com>
2018-06-07 09:09:24 -0700

    Clean up index

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './CommentBox';
+import CommentBox from './CommentBox';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
