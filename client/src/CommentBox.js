4cd4bb9ad58019f91528e8961417a8fa0c2e51c7
greg.mcgough <greg.mcgough@servicenow.com>
2018-07-18 16:30:44 -0700

    server and client are working able to post with curls and get

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -8,8 +8,38 @@ import './CommentBox.css';
 class CommentBox extends Component {
   constructor() {
     super();
-    this.state = { data: [] };
+    this.state = {
+      data: [],
+      error: null,
+      author: '',
+      text: '',
+    };
+    this.pollInterval = null;
   }
+
+  componentDidMount(){
+    this.loadCommentsFromServer();
+    if (!this.pollInterval) {
+      this.pollInterval = setInterval(this.loadCommentsFromServer, 2000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.pollInterval) clearInterval(this.pollInterval);
+    this.pollInterval = null;
+  }
+
+  loadCommentsFromServer = () => {
+    // fetch returns a promise. If you are not familiar with promises, see
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    fetch('/api/comments/')
+      .then(data => data.json())
+      .then((res) => {
+        if (!res.success) this.setState({ error: res.error });
+        else this.setState({ data: res.data });
+      });
+  }
+
   render() {
     return (
       <div className="container">
